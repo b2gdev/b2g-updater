d6a89e5ee375da9c7188f404b0a8062eb638cd2f
Ruvinda Dhambarage <ruvindad@zone24x7.com>
2015-12-01 14:09:42 +0530

    Initial commit

---
+++ src/test/java/com/zone24x7/b2gdev/b2g_updater/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.zone24x7.b2gdev.b2g_updater;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
