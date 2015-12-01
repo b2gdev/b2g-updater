90bde729ee74c62a9f872b3e6ba9f8709b7ecf79
Ruvinda Dhambarage <ruvindad@zone24x7.com>
2015-12-01 15:12:20 +0530

    Added button

--- src/main/java/com/zone24x7/b2gdev/b2g_updater/MainActivityFragment.java
+++ src/main/java/com/zone24x7/b2gdev/b2g_updater/MainActivityFragment.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements View.OnClickListener{
 
     public MainActivityFragment() {
     }
@@ -17,6 +19,18 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View  ret = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button rebootBtn = (Button) ret.findViewById(R.id.button);
+        rebootBtn.setOnClickListener(this);
+
+        return ret;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this.getContext(),
+                "Button pressed!",
+                Toast.LENGTH_SHORT).show();
     }
 }
