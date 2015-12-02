8e484e70e94664c2de25d7e749ba71e859165453
Ruvinda Dhambarage <ruvindad@zone24x7.com>
2015-12-02 16:47:34 +0530

    Better downloading

--- src/main/java/com/zone24x7/b2gdev/b2g_updater/DownloadFileFromURL.java
+++ src/main/java/com/zone24x7/b2gdev/b2g_updater/DownloadFileFromURL.java
@@ -3,19 +3,24 @@ package com.zone24x7.b2gdev.b2g_updater;
 
 import android.os.AsyncTask;
 import android.os.Environment;
+import android.os.PowerManager;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class DownloadFileFromURL extends AsyncTask<String, Integer, String> {
 
     MainActivityFragment container;
+    private PowerManager.WakeLock mWakeLock;
 
     public DownloadFileFromURL(MainActivityFragment f) {
         this.container = f;
@@ -25,49 +30,63 @@ public class DownloadFileFromURL extends AsyncTask<String, Integer, String> {
     @Override
     protected String doInBackground(String... params) {
 
-        int count;
+        OutputStream output = null;
+        InputStream input = null;
+        HttpURLConnection connection = null;
+
         try {
             URL url = new URL(params[0]);
-            URLConnection connection = url.openConnection();
+            connection = (HttpURLConnection)url.openConnection();
             connection.connect();
+
+            // expect HTTP 200 OK, so we don't mistakenly save error report
+            // instead of the file
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                return "Server returned HTTP " + connection.getResponseCode()
+                        + " " + connection.getResponseMessage();
+            }
 
             // this will be useful so that you can show a tipical 0-100%
             // progress bar
             int lengthOfFile = connection.getContentLength();
 
             // download the file
-            InputStream input = new BufferedInputStream(url.openStream(),
-                    8192);
+            input = connection.getInputStream();
 
             // Output stream
-            final File dest = new File(Environment
-                    .getExternalStorageDirectory().toString()
-                    +"/downloadedfile.kml");
-            OutputStream output = new FileOutputStream(dest);
+            final File dest = new File("/sdcard/file_name.extension");
+            output = new FileOutputStream(dest);
 
-            byte data[] = new byte[1024];
-
+            byte data[] = new byte[4096];
+            int count;
             long total = 0;
 
             while ((count = input.read(data)) != -1) {
+                // allow canceling with back button
+                if (isCancelled()) {
+                    input.close();
+                    return null;
+                }
                 total += count;
                 // publishing the progress....
-                // After this onProgressUpdate will be called
-                publishProgress((int) ((total * 100) / lengthOfFile));
-
-                // writing data to file
+                if (lengthOfFile > 0) // only if total length is known
+                    publishProgress((int) (total * 100 / lengthOfFile));
                 output.write(data, 0, count);
             }
 
-            // flushing output
-            output.flush();
-
-            // closing streams
-            output.close();
-            input.close();
-
         } catch (Exception e) {
-            Log.e("Error: ", e.getMessage());
+            return e.toString();
+        } finally {
+            try {
+                if (output != null)
+                    output.close();
+                if (input != null)
+                    input.close();
+            } catch (IOException ignored) {
+            }
+
+            if (connection != null)
+                connection.disconnect();
         }
 
         return null;
@@ -81,6 +100,12 @@ public class DownloadFileFromURL extends AsyncTask<String, Integer, String> {
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
+        // take CPU lock to prevent CPU from going off if the user
+        // presses the power button during download
+        PowerManager pm = (PowerManager) container.getActivity().getSystemService(container.getActivity().POWER_SERVICE);
+        mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                getClass().getName());
+        mWakeLock.acquire();
         container.showProgressBar();
     }
 
@@ -88,8 +113,17 @@ public class DownloadFileFromURL extends AsyncTask<String, Integer, String> {
     protected void onPostExecute(String result) {
         super.onPostExecute(result);
         // The activity can be null if it is thrown out by Android while task is running!
+
+        mWakeLock.release();
+
         if(container!=null && container.getActivity()!=null) {
             container.hideProgressBar();
+
+            if (result != null)
+                Toast.makeText(container.getActivity(), "Download error: " + result, Toast.LENGTH_LONG).show();
+            else
+                Toast.makeText(container.getActivity(),"File downloaded", Toast.LENGTH_SHORT).show();
+
             this.container = null;
         }
     }
